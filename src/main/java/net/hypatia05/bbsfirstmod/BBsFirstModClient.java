6f4b1254e9dec9b9467602a22afcfbe8fb763031
Hyphae05 <262043909+Hyphae05@users.noreply.github.com>
2026-03-05 03:32:36 -0800

    initial commit :3

---
+++ src/main/java/net/hypatia05/bbsfirstmod/BBsFirstModClient.java
@@ -0,0 +1,10 @@
+package net.hypatia05.bbsfirstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BBsFirstModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/hypatia05/bbsfirstmod/BBsFirstModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.hypatia05.bbsfirstmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BBsFirstModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
